e34c7381bfbc671e43b3a287d7f9d510099467a8
five8Nf <ashtonlimex@gmail.com>
2026-09-10 14:42:46 +0800

    Initial Commit

---
+++ Silly Is Life               Probably/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Silly Is Life               Probably
+//
+//  Created by Ashton Lim on 10/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
