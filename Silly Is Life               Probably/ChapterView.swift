c101a9c39a6cd53e6398b8d19639bec207f77a72
five8Nf <ashtonlimex@gmail.com>
2026-09-10 15:44:49 +0800

    first story

---
+++ Silly Is Life               Probably/ChapterView.swift
@@ -0,0 +1,18 @@
+//
+//  ChapterView.swift
+//  Silly Is Life               Probably
+//
+//  Created by Ashton Lim on 10/9/26.
+//
+
+import SwiftUI
+
+struct ChapterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChapterView()
+}
---
+++ Silly Is Life               Probably/Story1View.swift
@@ -0,0 +1,18 @@
+//
+//  Story1View.swift
+//  Silly Is Life               Probably
+//
+//  Created by Ashton Lim on 10/9/26.
+//
+
+import SwiftUI
+
+struct Story1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Story1View()
+}
